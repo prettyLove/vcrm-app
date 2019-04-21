0137dc83b91d1960139c7938b5fa2fa7d9754769
prettyLove <574286814@qq.com>
2019-04-21 21:40:55 +0800

    111

---
+++ src/views/add_user.vue
@@ -0,0 +1,143 @@
+<template>
+    <div class="add_user">
+         <div  @click="jump" >
+            <van-nav-bar title="添加客户" left-text="返回" left-arrow  right-text="保存" right-arrow class="bg_h">
+                <!-- <van-icon name="search" slot="right" /> -->
+            </van-nav-bar>
+        </div>
+        <van-row type="flex" justify="center" align="center">
+            <van-col span="6">
+                <img src="images/login/01.jpg" width="65%" class="img_f">
+            </van-col>
+            <van-col span='17'>
+                <van-cell-group>
+                    <van-field
+                        v-model="uname"
+                        type="textarea"
+                        placeholder="客户姓名"
+                        rows="1"
+                        autosize
+                        autofocus 
+                    />
+                    
+                    <van-field
+                        v-model="sex"
+                        type="textarea"
+                        placeholder="客户性别"
+                        rows="1"
+                        autosize
+                    />
+                    
+                    <van-field
+                        v-model="birthday"
+                        type="textarea"
+                        placeholder="客户生日"
+                        rows="1"
+                        autosize
+                    />
+                </van-cell-group>
+            </van-col> 
+        </van-row>
+        <van-cell-group class="group_pt" type="flex" justify="space-between">
+            <van-field
+           
+                v-model="phone"
+                clearable
+                placeholder="添加电话"
+                left-icon="plus"
+            />
+
+            <van-field
+                v-model="email"
+                type="email"
+                placeholder="添加邮箱"
+                left-icon="plus"
+            />
+            <van-field
+                v-model="addr"
+                type="addr"
+                placeholder="添加地址"
+                left-icon="plus"
+            />
+            <van-field
+                v-model="group"
+                type="group"
+                placeholder="添加分组"
+                left-icon="plus"
+            />
+            <van-field
+                v-model="circle"
+                type="circle"
+                placeholder="添加联络周期"
+                left-icon="plus"
+                autofocus 
+            />
+        </van-cell-group> 
+    </div>
+</template>
+<script>
+import { Cell, CellGroup,Row, Col,Field,Icon,NavBar } from 'vant';
+export default {
+    components:{
+        [Cell.name]:Cell,[CellGroup.name]:CellGroup,[Row.name]:Row,[Col.name]:Col,
+        [Field.name]:Field,[Icon .name]:Icon,[NavBar.name]:NavBar,
+    },
+    data(){
+       return{
+           uname:'',
+           sex:'',
+           birthday:'',
+           phone:'',
+           email:'',
+           addr:'',
+           group:'',
+           circle:'',
+       }
+    },
+    methods:{
+        jump(){
+
+        },
+    }
+
+}
+</script>
+<style scoped>
+  .group_pt{
+    padding-top:30px;
+ }
+  .img_f{
+      border-radius: 50%;
+  }
+  .van-cell{
+     padding-top:13px;
+     padding-bottom: 13px;
+  }
+  .group_pt>.van-cell~.van-cell{
+     padding-top:18px;
+     padding-bottom: 18px;
+  }
+ 
+</style>
+
+<style>
+   .van-cell__value{
+       display: flex;
+   }
+    .van-field__left-icon{
+    border-radius: 50%;
+    background-color: #26a2ff;
+    color: #fff;
+    margin-right: 10px
+  }
+   .bg_h{
+       background-color: #26a2ff!important; 
+  
+    }
+    .van-nav-bar__arrow,.van-nav-bar__title,.van-nav-bar__text,.van-icon-search{
+        color:#fff!important;
+    }
+  
+</style>
+
+
---
+++ src/views/user.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="user">
+        <!-- 头部组件 -->
+       <div  @click="jump" >
+            <van-nav-bar title="添加客户" left-text="返回" left-arrow  right-text="保存" right-arrow class="bg_h">
+                <van-icon name="user-o" slot="right" @click="showDialog"/>
+            </van-nav-bar>
+            <van-dialog
+                v-model="show"
+                title="添加用户？"
+                show-cancel-button
+                :beforeClose="beforeClose"
+                >
+            </van-dialog>
+        </div>
+        <van-cell-group class="margin_lr">
+            <van-field
+                placeholder="请输入要搜索的关键字"
+                left-icon="search"
+            />
+        </van-cell-group>
+        <van-cell-group >
+             <van-cell is-link>
+                <div slot="title" border="1px solid" @click="jump">
+                    <i class="fa fa-address-book-o fa-2x" aria-hidden="true"></i>
+                    <span class="custom-text">线索</span>
+                </div>
+            </van-cell>
+        </van-cell-group>
+    </div>
+</template>
+<script>
+import { Cell, CellGroup,Row, Col,Field,Icon,NavBar,Search} from 'vant';
+export default {
+     components:{
+        [Cell.name]:Cell,[CellGroup.name]:CellGroup,[Row.name]:Row,[Col.name]:Col,
+        [Field.name]:Field,[Icon .name]:Icon,[NavBar.name]:NavBar,[Search.name]:Search,
+    },
+    data(){
+       return{
+        show:false
+       }
+    },
+    methods:{
+        jump(){},
+        showDialog(){
+            this.show=true;
+        },
+         beforeClose(action,done) {
+            if(action=='confirm'){
+                this.$router.push('/add_user')
+            }else{
+                done();
+            }
+        }
+    },
+    mounted() {//动态设置单个组件页面的背景颜色
+            document.querySelector('body').setAttribute('style', 'background-color:#f8f8f8')
+        },
+    beforeDestroy() {
+        document.querySelector('body').removeAttribute('style')
+    },
+
+    
+}
+</script>
+<style scoped>
+  .margin_lr{
+      margin-left: 15px;
+      margin-right: 15px
+  }
+  .van-cell__value{
+      display: flex;
+  }
+  
+</style>
+<style>
+.bg_h{
+       background-color: #26a2ff!important; 
+  
+    }
+    .van-nav-bar__arrow,.van-nav-bar__title,.van-nav-bar__text,.van-icon-user-o{
+        color:#fff!important;
+        font-size: 16px;
+    }
+    .van-field{
+      padding:0px!important;
+      margin-top: 10px;
+      margin-bottom: 10px
+  }
+  .van-cell__value--alone{
+      display: flex
+  }
+
+</style>
+
+
+
