4ac5f075ca69a361ead0de191260ca7770f3f1a5
prettyLove <574286814@qq.com>
2019-04-21 21:50:08 +0800

    22

--- src/router.js
+++ src/router.js
@@ -17,10 +17,6 @@ export default new Router({
     //mode: "history",
     //base: process.env.BASE_URL,
     routes: [
-        { path: "/", name: "login",component: Login},
-        {path: "/home",name: "home",component: Home},
-        //{path:"/index", name:"index",component:index},
-        //{path:"/clue",name:"clue",component:clue},
         {
             path: "/",
             name: "login",
@@ -53,7 +49,7 @@ export default new Router({
             ]
         },
         {path: "/user", name: "user", component: user},
-        //{path: "/clue", name: "clue", component: clue},
+
         {path:"/clue_group",name:"clue_group",component:clue_group},
     ]
 });
