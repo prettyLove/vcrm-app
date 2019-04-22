c5a87b62ffeb53dd0d297cd4405623bbba2f6415
master <574286814@qq.com>
2019-04-22 09:53:39 +0800

    1

--- src/router.js
+++ src/router.js
@@ -32,14 +32,6 @@ export default new Router({
                     path: 'index',
                     name: 'index',
                     component: index,
-                    // redirect: '/home/indexHeader/index',
-                    // children:[
-                    //     {
-                    //         path: 'index',
-                    //         name: 'index',
-                    //         component: index
-                    //     }
-                    // ]
                 },
                 {
                     path: 'clue',
